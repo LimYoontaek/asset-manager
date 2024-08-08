da2232a7faf470c3b6809f32857fdf62f37219a8
LimYoontaek <lim.yoontaek@triplecomma.co.kr>
2024-08-08 15:35:24 +0900

    add devtool middleware

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,13 @@
 import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 
 interface LoginState {
   isLogin: boolean;
   setIsLogin: (isLogin: boolean) => void;
 }
-export const useLoginStore = create<LoginState>((set) => ({
-  isLogin: false,
-  setIsLogin: (isLogin) => set(() => ({ isLogin })),
-}));
+export const useLoginStore = create<LoginState>()(
+  devtools((set) => ({
+    isLogin: false,
+    setIsLogin: (isLogin) => set(() => ({ isLogin })),
+  })),
+);
