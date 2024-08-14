6611f9004fdb9570a0936ec38d423d17f77ff3c0
LimYoontaek <lim.yoontaek@triplecomma.co.kr>
2024-08-14 16:34:19 +0900

    add json data store

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,34 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import {
-  DocumentData,
-  onSnapshot,
-  QueryDocumentSnapshot,
-} from "firebase/firestore";
-import { BadgeType } from "@type/badges";
 import AddBadge from "@src/AddBadge";
 import EditBadge from "@src/EditBadge";
-import { auth, badgeQuery, storageRef } from "@util/firestoreSetup";
+import { auth, jsonRef } from "@util/firestoreSetup";
 import Login from "./Login";
 import { onAuthStateChanged } from "firebase/auth";
-import { useLoginStore } from "./store/store";
-import { ref, uploadBytes } from "firebase/storage";
+import {
+  JsonDataType,
+  useJsonDataStore,
+  useLoginStore,
+  useMenuStore,
+  useSelectedDataStore,
+} from "./store/store";
+import { getBlob } from "firebase/storage";
 import Menu from "@src/Menu";
+import SubMenuList from "@src/SubMenuList";
 
 const App = () => {
-  const [data, setData] =
-    useState<QueryDocumentSnapshot<DocumentData, DocumentData>[]>();
-  const [selectedBadge, setSelectedBadge] = useState<QueryDocumentSnapshot<
-    DocumentData,
-    DocumentData
-  > | null>();
+  const selectedBadge = useSelectedDataStore.use.selectedBadge();
+  const selectedMenu = useMenuStore.use.selectedMenu();
+
   const [addBadge, setAddBadge] = useState<boolean>(false);
-  const [isExporting, setIsExporting] = useState(false);
+  // const [isExporting, setIsExporting] = useState(false);
 
   const isLogin = useLoginStore.use.isLogin();
   const setIsLogin = useLoginStore.use.setIsLogin();
 
-  const closeBadgeDetail = useCallback(() => {
-    setSelectedBadge(null);
-  }, []);
+  const setJsonData = useJsonDataStore.use.setJsonData();
 
-  const exportBadges = () => {
-    setIsExporting(true);
-
-    const badgeList = data?.map((badgeDoc) => JSON.stringify(badgeDoc.data()));
-    const badgeBlob = new Blob([JSON.stringify(badgeList)], {
-      type: "application/json",
-    });
-    const badgeRef = ref(storageRef, "badges.json");
-
-    uploadBytes(badgeRef, badgeBlob)
-      .then(() => {
-        console.log("badges uploaded.");
-      })
-      .catch((e) => {
-        console.error("badge upload failed.", e);
-      })
-      .finally(() => {
-        setIsExporting(() => false);
-      });
-  };
-
-  useEffect(() => {
-    // Firestore realtime listen
-    const unsub = onSnapshot(badgeQuery, (querySnapshot) => {
-      setData(querySnapshot.docs);
-    });
-
-    return () => {
-      unsub();
-    };
-  }, []);
+  const exportBadges = () => {};
 
   useEffect(() => {
     const unsub = onAuthStateChanged(auth, (user) => {
@@ -79,6 +45,28 @@ const App = () => {
     };
   }, [setIsLogin]);
 
+  useEffect(() => {
+    if (isLogin) {
+      getBlob(jsonRef)
+        .then((data: Blob) => {
+          const jsonParse = async () => {
+            const jsonData = JSON.parse(await data.text()) as {
+              json: { badges: JsonDataType };
+            };
+            console.log(`badges.json downloaded.`, jsonData);
+            setJsonData(jsonData.json.badges);
+          };
+
+          jsonParse().catch((e) => {
+            console.error(`parsing error.`, e);
+          });
+        })
+        .catch((e) => {
+          console.error(`badges.json download error`, e);
+        });
+    }
+  }, [isLogin, setJsonData]);
+
   return (
     <>
       <h1 className="w-full text-center">Asset Manager</h1>
@@ -88,7 +76,7 @@ const App = () => {
           <button
             className="disabled:border-none disabled:bg-stone-50 disabled:text-slate-300"
             onClick={exportBadges}
-            disabled={isExporting}
+            disabled={true}
           >
             Export
           </button>
@@ -100,43 +88,12 @@ const App = () => {
             <Menu />
           </div>
           <div className="sm:w-[40%]">
-            <div className="mb-4 flex items-center sm:text-center">
-              <p className="grow">Badges({data?.length})</p>
-              <button
-                className="h-fit w-fit"
-                onClick={() => {
-                  setAddBadge((current) => !current);
-                  closeBadgeDetail();
-                }}
-              >
-                Add
-              </button>
-            </div>
-            {data &&
-              data.map((badgeDoc) => {
-                const badge = badgeDoc.data() as BadgeType;
-                return (
-                  <p
-                    className="my-4 cursor-pointer border-b-2"
-                    key={badge.id}
-                    onClick={() => {
-                      if (selectedBadge !== badgeDoc) {
-                        setSelectedBadge(badgeDoc);
-                      } else {
-                        closeBadgeDetail();
-                      }
-                    }}
-                    aria-hidden="true"
-                  >{`${badge.title} / ${badge["sub-title"]}`}</p>
-                );
-              })}
+            <SubMenuList setAddBadge={setAddBadge} />
           </div>
           <div className="sm:w-[40%]">
             {!addBadge && selectedBadge && (
               <EditBadge
-                key={selectedBadge.id}
-                selectedBadge={selectedBadge}
-                closeBadgeDetail={closeBadgeDetail}
+                key={`${selectedMenu}-${typeof selectedBadge == "object" ? ("id" in selectedBadge ? selectedBadge.id : "title" in selectedBadge ? selectedBadge.title : "") : ""}`}
               />
             )}
             {addBadge && <AddBadge setAddBadge={setAddBadge} />}
--- src/Menu.tsx
+++ src/Menu.tsx
@@ -1,38 +1,18 @@
 import { RichTreeView } from "@mui/x-tree-view";
-import { MENU, MenuType } from "@src/assets/menu";
-import { database } from "@src/utils/firestoreSetup";
-import {
-  collection,
-  DocumentData,
-  getDocs,
-  query,
-  QuerySnapshot,
-} from "firebase/firestore";
-
+import { MENU, MenuListType, MenuType } from "@src/assets/menu";
+import { useMenuStore, useSelectedDataStore } from "@src/store/store";
 const isItemDisabled = (item: MenuType) => !item.active;
 
 const Menu = () => {
+  const setMenu = useMenuStore.use.setMenu();
+  const resetSelectedData = useSelectedDataStore.use.resetSelectedData();
   return (
     <RichTreeView
       items={MENU}
       isItemDisabled={isItemDisabled}
-      onItemClick={(e: React.MouseEvent, itemId: string) => {
-        console.log(itemId);
-
-        //TODO: 배지 리스트 컴포넌트 생성
-        //TODO: 메뉴 컴포넌트에서 배지 리스트 컴포넌트에 선택된 아이템 전달(zustand?)
-        //TODO: 배지 리스트 컴포넌트에서 firestore 데이터 가져오기
-        const getList = async () => {
-          await getDocs(
-            query(collection(database, `badges/${itemId}/list`)),
-          ).then((snapShot: QuerySnapshot<DocumentData, DocumentData>) => {
-            console.log(snapShot.docs);
-          });
-        };
-
-        getList().catch((e) => {
-          console.error(`get list failed`, e);
-        });
+      onItemClick={(_e: React.MouseEvent, itemId: string) => {
+        setMenu(itemId as MenuListType);
+        resetSelectedData();
       }}
     />
   );
