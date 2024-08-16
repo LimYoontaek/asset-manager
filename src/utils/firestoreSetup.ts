e41dcac18adcfa90ce7578b371a6ba4e766186c7
LimYoontaek <lim.yoontaek@triplecomma.co.kr>
2024-08-16 12:05:57 +0900

    add export, publish

--- src/utils/firestoreSetup.ts
+++ src/utils/firestoreSetup.ts
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, query, orderBy } from "firebase/firestore";
 import { firebaseConfig } from "../../config/firebase";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getStorage, ref, StorageReference } from "firebase/storage";
+import { getStorage } from "firebase/storage";
 
 const app = initializeApp(firebaseConfig);
 
@@ -15,7 +15,10 @@ export const badgeQuery = query(badgeRef, orderBy("id"));
 export const loginProvider = new GoogleAuthProvider();
 export const auth = getAuth();
 
-const storage = getStorage();
-export const storagePath = "goldspoon/qa";
-export const storageRef: StorageReference = ref(storage, storagePath);
-export const jsonRef = ref(storageRef, "badges.json");
+export const storage = getStorage();
+export const gsQAstoragePath = "goldspoon/qa";
+export const gsLivestoragePath = "goldspoon/live";
+export const tmQAstoragePath = "themembers/live";
+export const tmLivestoragePath = "themembers/live";
+
+export const fileNameRegex = new RegExp(/badges_(\d+)_(\d+).json/);
