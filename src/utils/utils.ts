1f3891a37cb2792a28e4d61d9bb54759e7df6c00
LimYoontaek <lim.yoontaek@triplecomma.co.kr>
2024-08-16 15:02:35 +0900

    add get date util

---
+++ src/utils/utils.ts
@@ -0,0 +1,13 @@
+export const getDate = (date: string) => {
+  const newDate = new Date(date);
+  if (isNaN(newDate.getTime())) return "";
+  console.log(newDate);
+  return new Intl.DateTimeFormat("ko-kr", {
+    year: "2-digit",
+    month: "numeric",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    hour12: false,
+  }).format(newDate);
+};
